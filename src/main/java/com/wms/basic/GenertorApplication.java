4de63fd4eab1ade1e7e38c028a85d91b5c0ece48
wangjiancheng721 <a3710178@qq.com>
2022-03-14 17:26:57 +0800

    first commit

---
+++ src/main/java/com/wms/basic/GenertorApplication.java
@@ -0,0 +1,13 @@
+package com.wms.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenertorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenertorApplication.class, args);
+    }
+
+}
